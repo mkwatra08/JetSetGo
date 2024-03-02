9ae6c450e8268ee176320cd1e2dc317148d64f57
Tariq <tariqpasha04@gmail.com>
2024-03-02 23:42:02 +0530

    django implementation

---
+++ jetsetgo/travel/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from datetime import datetime
+from travel.models import contact
+from django.contrib import messages
+def home(request):
+    return render(request, 'body.html')
+def about(request):
+    return render(request,'#about')
+def services(request):
+    return render(request,'#services')
+def blog(request):
+    return render(request, 'blog.html')
+def portfolio(request):
+    return render(request, 'portfolio.html')
+def adventure(request):
+    return render(request, 'adventure.html')
+def team(request):
+    return render(request, '#team')
+def Contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        texts = request.POST.get('texts')
+        Contact = contact(name = name, email = email, texts = texts, date = datetime.today())
+        Contact.save()
+        messages.success(request, "Message sent Succesfully.")
+    return render(request, '#contact')
+# Create your views here.
